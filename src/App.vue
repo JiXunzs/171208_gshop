c48ac66292adaf2778bff437fea726fedb74649c
lixiaolong <13971379657@163.com>
2019-04-17 11:26:56 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <router-view></router-view>
+      <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+    </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  export default {
+    name: 'Vue',
+    components:{
+      'FooterGuide': FooterGuide
+    }
+  }
+</script>
+
+<style>
+
+</style>
